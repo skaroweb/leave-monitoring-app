acccf08e5690196c1e3d12c2669f26a15e4ca496
AngappanMERN <angappan83000@gmail.com>
2026-04-07 11:18:32 +0530

    server URL implement

--- server/index.js
+++ server/index.js
@@ -23,7 +23,10 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 
 
-app.use(cors());
+app.use(cors({
+  origin: "https://leave-monitoring-app-4izu.onrender.com",
+  credentials: true
+}));
 
 
 
